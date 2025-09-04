5ae43ce603f2e06cd3af24caea384584df368f17
Rodrigo Concha <externo.ryc63@consorcio.cl>
2025-09-04 17:06:35 -0400

    menos ordenado

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
-import { Fragment } from "react";
-
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No items found.</p>
+      </>
+    );
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li key={item}> {item}</li>
         ))}
       </ul>
     </>
