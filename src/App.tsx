c0f9c355a94ae9114c629889a5a671d83aa238eb
Rodrigo Concha <externo.ryc63@consorcio.cl>
2025-09-09 15:15:04 -0300

    alert 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import Alert from "./components/Alert";
 function App() {
-  return <div className="alert alert-primary"> Alert </div>;
+  return (
+    <div>
+      <Alert>Hello World</Alert>
+    </div>
+  );
 }
 
 export default App;
